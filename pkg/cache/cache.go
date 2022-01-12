d4d41fcffdb794a392d6711d0866ae31677d9c37
Chris <chris@clr.is>
2022-01-12 01:11:41 +0000

    `Cacher` interface has been added, along with an implementation: `CacheImpl`, which wraps patricknm's `go-cache`.

---
+++ pkg/cache/cache.go
@@ -0,0 +1,121 @@
+package cache
+
+import (
+	gocache "github.com/patrickmn/go-cache"
+	"time"
+)
+
+type cacheUnit struct {
+	data    interface{}
+	isValid func(u *cacheUnit) bool
+}
+
+// NewCacheUnit creates a new cacheable chunk of data. Optionally a function
+// `isValid` can be provided to check if a `*cacheUnit` is valid. If this
+// function fails the cache will purge the key.
+func NewCacheUnit(data interface{}, isValid func(u *cacheUnit) bool) *cacheUnit {
+	return &cacheUnit{
+		data:    data,
+		isValid: isValid,
+	}
+}
+
+// Cacher represents all cache implementations, barebones interoperable
+// functions, for any implementation that may be used down the line.
+type Cacher interface {
+	// Put sets, if not present, the key to the `*cache.Unit`, with default
+	// expiry. May return an error depending on impl.
+	Put(key string, unit *cacheUnit) error
+	// PutExpiry sets, if not present, the key to the `*cache.Unit`, with
+	// given expiry. May return an error depending on impl.
+	PutExpiry(key string, unit *cacheUnit, expiry time.Time) error
+	// Set sets, the key to the `*cache.Unit`, present or not, with default
+	// expiry. May return an error depending on impl.
+	Set(key string, unit *cacheUnit) error
+	// SetExpiry sets, the key to the `*cache.Unit`, present or not, with
+	// given expiry. May return an error depending on impl.
+	SetExpiry(key string, unit *cacheUnit, expiry time.Time) error
+    // Get returns the key's unit's value if valid. Checks the `isValid`
+	// function & expiry times. Bool indicates if present (& valid).
+	Get(key string) (interface{}, bool)
+	// GetDefault returns the key's unit's value if valid. Checks the `isValid`
+	// function & expiry times. If not present (or valid) `d` is returned.
+	GetDefault(key string, d interface{}) interface{}
+    // Delete will remove the key from the cache. May return an error depending
+	// on impl.
+	Delete(key string) error
+}
+
+// CacheImpl implements patrickmn's `go-cache`.
+type CacheImpl struct {
+	cache *gocache.Cache
+}
+
+func NewCache(defaultTtl *time.Duration, defaultClean *time.Duration) *CacheImpl {
+	var ttl, clean time.Duration
+
+	if defaultTtl == nil {
+		ttl = gocache.NoExpiration
+	}
+
+	if defaultClean == nil {
+		clean = time.Minute * 15
+	}
+
+	return &CacheImpl{
+		cache: gocache.New(ttl, clean),
+	}
+}
+
+func (c *CacheImpl) Put(key string, unit *cacheUnit) error {
+	return c.cache.Add(key, unit, gocache.DefaultExpiration)
+}
+
+func (c *CacheImpl) PutExpiry(key string, unit *cacheUnit, expiry time.Time) error {
+	return c.cache.Add(key, unit, expiry.Sub(time.Now()))
+}
+
+func (c *CacheImpl) Set(key string, unit *cacheUnit) error {
+	c.cache.Set(key, unit, gocache.DefaultExpiration)
+	return nil
+}
+
+func (c *CacheImpl) SetExpiry(key string, unit *cacheUnit, expiry time.Time) error {
+	c.cache.Set(key, unit, expiry.Sub(time.Now()))
+	return nil
+}
+
+func (c *CacheImpl) Get(key string) (interface{}, bool) {
+	var ret interface{}
+
+	unit, found := c.cache.Get(key)
+	if !found || unit == nil {
+		return nil, false
+	}
+
+	cast, ok := unit.(*cacheUnit)
+	if ok {
+		if cast.isValid != nil && !cast.isValid(cast) {
+			_ = c.Delete(key)
+			return nil, false
+		}
+
+		ret = cast.data
+	}
+
+	return ret, ok
+}
+
+func (c *CacheImpl) GetDefault(key string, d interface{}) interface{} {
+	ret, ok := c.Get(key)
+	if !ok {
+		ret = d
+	}
+
+	return ret
+}
+
+func (c *CacheImpl) Delete(key string) error {
+	c.cache.Delete(key)
+	return nil
+}
---
+++ pkg/cache/cache_test.go
@@ -0,0 +1,111 @@
+package cache
+
+import (
+	"testing"
+	"time"
+)
+
+// Some implementation functions don't actually return errors, despite indicating
+// Due to how patricknm's library is written. So that's why some are ignored.
+
+func TestCacheImpl_Delete(t *testing.T) {
+	c := NewCache(nil, nil)
+	_ = c.Set("key", NewCacheUnit(5, nil))
+
+	_, present := c.Get("key")
+	if !present {
+		t.Errorf("cache unit not present after adding")
+	}
+
+	_ = c.Delete("key")
+	if _, present2 := c.Get("key"); present2 {
+		t.Errorf("cache unit present after deleting")
+	}
+}
+
+func TestCacheImpl_Get(t *testing.T) {
+	c := NewCache(nil, nil)
+
+	_ = c.Set("one", NewCacheUnit(1, nil))
+	if one, present := c.Get("one"); !present {
+		t.Errorf("cache unit not preent after adding")
+	} else if one != 1 {
+		t.Errorf("cache unit value not correct, wanted %v got %v", 1, one)
+	}
+
+	if _, present := c.Get("two"); present {
+		t.Errorf("cache unit apparently present when it does not exist")
+	}
+}
+
+func TestCacheImpl_GetDefault(t *testing.T) {
+	c := NewCache(nil, nil)
+
+	if val := c.GetDefault("key", 5); val != 5 {
+		t.Errorf("value from GetDefault was %v wanted %v", val, 5)
+	}
+}
+
+func TestCacheImpl_Put(t *testing.T) {
+	c := NewCache(nil, nil)
+	_ = c.Put("key", NewCacheUnit(2, nil)) // impl never errors
+
+	if val, ok := c.Get("key"); !ok {
+		t.Errorf("cache unit not present after adding")
+	} else if val != 2 {
+		t.Errorf("value after Put is incorrect")
+	}
+}
+
+func TestCacheImpl_PutExpiry(t *testing.T) {
+	c := NewCache(nil, nil)
+	_ = c.PutExpiry("key", NewCacheUnit(2, nil), time.Now().Add(time.Millisecond * 250)) // impl never errors
+
+	if val, ok := c.Get("key"); !ok || val != 2 {
+		t.Errorf("cache unit not (correctly) present immediately after adding, wanted %v got %v", 2, val)
+	}
+
+	time.Sleep(time.Millisecond * 300)
+	if _, ok := c.Get("key"); ok {
+		t.Errorf("cache unit present after expiry time, wanted nothing")
+	}
+}
+
+func TestCacheImpl_Set(t *testing.T) {
+	c := NewCache(nil, nil)
+
+	_ = c.Set("key", NewCacheUnit(7, nil))
+	if val, ok := c.Get("key"); !ok {
+		t.Errorf("cache unit not present after adding")
+	} else if val != 7 {
+		t.Errorf("value incorrect got %v wanted %v", val, 7)
+	}
+
+	_ = c.Set("key", NewCacheUnit(8, nil))
+	if val, ok := c.Get("key"); !ok {
+		t.Errorf("cache unit not present after overwriting")
+	} else if val != 8 {
+		t.Errorf("cache unit has not overwritten value, got %v wanted %v", val, 8)
+	}
+}
+
+func TestCacheImpl_SetExpiry(t *testing.T) {
+	c := NewCache(nil, nil)
+
+	_ = c.Set("key", NewCacheUnit(7, nil))
+	if val, ok := c.Get("key"); !ok {
+		t.Errorf("cache unit not present after adding")
+	} else if val != 7 {
+		t.Errorf("value incorrect got %v wanted %v", val, 7)
+	}
+
+	_ = c.SetExpiry("key", NewCacheUnit(8, nil), time.Now().Add(time.Millisecond * 250))
+	if val, ok := c.Get("key"); !ok || val != 8 {
+		t.Errorf("cache unit not (correctly) present immediately after overwriting, wanted %v got %v", 8, val)
+	}
+
+	time.Sleep(time.Millisecond * 300)
+	if _, ok := c.Get("key"); ok {
+		t.Errorf("cache unit present after expiry")
+	}
+}
