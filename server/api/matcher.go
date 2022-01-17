fdd5919ddf874e5980fa3152d7005492e5543f0f
Chris <chris@clr.is>
2022-01-17 14:58:36 +0000

    `MultiMatcher`

--- server/api/matcher.go
+++ server/api/matcher.go
@@ -4,10 +4,50 @@ import (
 	"github.com/clr1107/facade/server/server"
 	"net/url"
 	"strings"
+	"sync"
 )
 
 type Matcher interface {
 	Match(request []byte) server.Pipe
+}
+
+// ---------- MultiMatcher ----------
+
+type MultiMatcher struct {
+	matchers []Matcher
+	mux *sync.RWMutex
+}
+
+func NewMultiMatcher(matchers []Matcher) *MultiMatcher {
+	return &MultiMatcher{
+		matchers: matchers,
+		mux: new(sync.RWMutex),
+	}
+}
+
+func (multiMatcher *MultiMatcher) AppendMatcher(matcher Matcher) {
+	multiMatcher.mux.Lock()
+	multiMatcher.matchers = append(multiMatcher.matchers, matcher)
+	multiMatcher.mux.Unlock()
+}
+
+func (multiMatcher *MultiMatcher) PrependMatcher(matcher Matcher) {
+	multiMatcher.mux.Lock()
+	multiMatcher.matchers = append([]Matcher{matcher}, multiMatcher.matchers...)
+	multiMatcher.mux.Unlock()
+}
+
+func (multiMatcher *MultiMatcher) Match(request []byte) server.Pipe {
+	multiMatcher.mux.RLock()
+	defer multiMatcher.mux.RUnlock()
+
+	for _, matcher := range multiMatcher.matchers {
+		if x := matcher.Match(request); x != nil {
+			return x
+		}
+	}
+
+	return nil
 }
 
 // ---------- RedirectHostMatcher ----------
